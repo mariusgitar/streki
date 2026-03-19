72eff507c2ca3362fe6bda3829815477cca39950
mariusgitar <161877670+mariusgitar@users.noreply.github.com>
2026-03-19 23:38:23 +0100

    Change Lottie animation source to local file

--- src/components/GeneratingAnimation.jsx
+++ src/components/GeneratingAnimation.jsx
@@ -6,7 +6,7 @@ function GeneratingAnimation() {
     <div className="mt-8 flex w-full max-w-xl flex-col items-center justify-center text-center">
       <div className="flex h-full min-h-[356px] w-full items-center justify-center">
         <DotLottieReact
-          src="https://lottie.host/572483a5-0ca6-4189-bb2b-5da702eb1b94/jsvdwLW1Z6.lottie"
+          src="src/drawing.lottie"
           loop
           autoplay
           className="h-full w-full"
