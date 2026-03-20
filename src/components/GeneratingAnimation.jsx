0dcea41b1721969f1f11c9dfdb5bbeb293bd27ef
mariusgitar <161877670+mariusgitar@users.noreply.github.com>
2026-03-20 08:02:44 +0100

    Remove committed dotLottie wasm asset

--- src/components/GeneratingAnimation.jsx
+++ src/components/GeneratingAnimation.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { DotLottieReact, setWasmUrl } from '@lottiefiles/dotlottie-react'
+
+const localDotLottieWasmUrl = new URL(
+  '../../node_modules/@lottiefiles/dotlottie-web/dist/dotlottie-player.wasm',
+  import.meta.url,
+).href
+
+setWasmUrl(localDotLottieWasmUrl)
 
 function GeneratingAnimation() {
   return (
     <div className="mt-8 flex w-full max-w-xl flex-col items-center justify-center text-center">
       <div className="flex h-full min-h-[356px] w-full items-center justify-center">
         <DotLottieReact
-          src={`${window.location.origin}/drawing.lottie`}
+          src="/drawing.lottie"
           loop
           autoplay
           className="h-full w-full"
